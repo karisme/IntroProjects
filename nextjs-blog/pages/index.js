31a1e3a1c3936466e9538b26f422626c748b6f56
Ali Ka <kalikibbel@gmail.com>
2020-08-09 19:09:45 -0700

    fetching data externally and static pre-rendering

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,8 +3,9 @@ import Layout, { siteTitle } from "../components/layout"
 import utilStyles from "../styles/utils.module.css"
 import Link from 'next/link'
 import TypeIt from "typeit-react";
+import { getSortedPostsData } from '../lib/fetch-parse.js'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,10 +16,32 @@ export default function Home() {
           <TypeIt>Wannabe Software Engineer, trying to make it. Don't @ me after the come up.</TypeIt>
         </p>
         <p>
-          Created this using the <a href="https://nextjs.org"> Next.js</a> framework. Looking forward to learning more about it.
+          Created this using the <a href="https://nextjs.org"> Next.js</a> framework. Looking forward to making more cool stuff with it.
         </p>
-        <Link href="/posts/first-post"><a> First and only Route lol</a></Link>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Posts</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
